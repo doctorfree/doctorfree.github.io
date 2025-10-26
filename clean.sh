9c59e6bc9fc7da0416be5eafdd37577041f76d19
ron.record <ron.record@hornblower.com>
2025-10-26 10:26:26 -0700

    Move shell scripts into tools, rename scripts, cd to top of repo when running scripts

--- clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-have_bundle=$(type -p bundle)
-if [ "${have_bundle}" ]; then
-  bundle exec jekyll clean
-else
-  rm -rf _site .sass-cache .jekyll-cache
-  rm -f .jekyll-metadata .DS_Store
-fi
